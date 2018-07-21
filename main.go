badb1f347e54fe194ac328d0f2ae4f90683f5546
zombieht <zombie19931113@gmail.com>
2018-07-21 20:14:56 +0800

    完成

--- main.go
+++ main.go
@@ -10,11 +10,22 @@ import (
 type rq struct {
 	Address string `json:"address"`
 }
-
+type Hero struct {
+    Id     int `json:"id"`
+    Name   string `json:"name"`
+}
 
 func main() {
 	router := gin.Default()
-	router.POST("/post", func(c *gin.Context) {
+	// router.LoadHTMLGlob("/")
+	router.LoadHTMLFiles("index.html")
+	router.GET("/", func(c *gin.Context) {
+        c.HTML(http.StatusOK, "index.tmpl",gin.H{
+            "title": "index",
+        })
+    })
+
+	router.POST("/api/post", func(c *gin.Context) {
 		var json rq
 		if err := c.BindJSON(&json);
 		err != nil {
@@ -27,6 +38,21 @@ func main() {
 		}
 		c.JSON(http.StatusOK, json.Address)
 	})
+
+	router.GET("/api/heroes", func(c *gin.Context) {
+		hero := []Hero{
+			// Id:     1,
+			// Name:  "name",
+			{1,"t1"},
+			{2,"t2"},
+			{3,"t3"},
+			{4,"t4"},
+		}
+		// name := c.Param("name")
+		//c.String(http.StatusOK, "Hello %s", name)
+		c.JSON(http.StatusOK, hero)
+	})
+
 
 	router.Run(":8080")
 }
